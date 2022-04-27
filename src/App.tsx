2998ac26d20233f89443d11b401d24af232f4aa9
Dave Calderwood <davidhcalderwood@gmail.com>
2022-04-27 15:07:19 -0400

    Components and data models in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
+
 
 const App = () => {
+
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript'),
+  ];
+
   return (
     <div>
+      <Todos items={todos} />
     </div>
   );
 }
